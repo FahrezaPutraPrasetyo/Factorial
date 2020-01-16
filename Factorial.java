fd72ce360e2046b2bae86a526c3a9f543b25c85c
FahrezaPutraPrasetyo <fahrezaputraprasetyo999@gmail.com>
2020-01-16 12:08:26 +0700

    change number

--- Factorial.java
+++ Factorial.java
@@ -1,6 +1,6 @@
 public class Factorial {
     public static void main(String[] args) {
-        int num = 7, i = 1;
+        int num = 6, i = 1;
         long factorial = 1;
         while(i <= num)
         {
